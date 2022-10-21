4c5120b1f39b5de79cb79ef1e8a3a90f42288276
Faza Nurfaizi <nurfaizi.faza@gmail.com>
2022-10-21 08:14:31 +0700

    Categories Page

--- resources/js/views/category/index.vue
+++ resources/js/views/category/index.vue
@@ -2,6 +2,14 @@
     <div>
         <div class="row mb-3">
             <div class="col-7">
+                <el-button
+                    v-if="multipleSelection.length > 0"
+                    :icon="Delete"
+                    circle
+                    class="mx-2 text-danger bg-transparent border-0"
+                    @click="handleDelete"
+                />
+
                 <el-input
                     v-model="searchQuery"
                     placeholder="Search"
@@ -15,6 +23,7 @@
                 <button
                     class="btn btn-blue-gradient w-25"
                     type="button"
+                    @click="formVisible = true"
                 >
                     <span class="text-sm">
                         + Add New
@@ -38,19 +47,19 @@
                     <el-button
                         type="primary"
                         size="small"
-                        @click.prevent="detailRow(scope.row)"
+                        @click.prevent="editRow(scope.row)"
                         :icon="EditPen"
                     />
                     <el-button
                         type="primary"
                         size="small"
-                        @click.prevent="deleteRow(scope.row)"
-                        :icon="EditPen"
+                        @click.prevent="detailRow(scope.row)"
+                        :icon="View"
                     />
                 </template>
             </el-table-column>
-            <el-table-column prop="name" label="Category name" sortable />
-            <el-table-column prop="date" label="Created date" sortable />
+            <el-table-column prop="name" label="Name" sortable />
+            <el-table-column prop="slug" label="Slug" sortable />
         </el-table>
 
         <div class="d-flex justify-content-between">
@@ -59,13 +68,20 @@
                 layout="prev, pager, next"
                 v-model:currentPage="currentPage"
                 v-model:page-size="pageSize"
-                :page-sizes="[10, 25, 50]"
-                :total="1000"
+                :page-sizes="[5, 10, 25, 50]"
+                :total="totalData"
                 @size-change="handleSizeChange"
                 @current-change="handleCurrentChange"
             />
             <div class="px-2">
-                <el-select v-model="pageSize" class="m-2" placeholder="Select" size="large" style="width: 75px;">
+                <el-select
+                    v-model="pageSize"
+                    class="m-2"
+                    placeholder="Select"
+                    size="large"
+                    @change="handleSizeChange"
+                    style="width: 75px;"
+                >
                     <el-option
                         v-for="item in pageOptions"
                         :key="item"
@@ -73,19 +89,86 @@
                         :value="item"
                     />
                 </el-select>
-                <span class="text-gray">Showing 1-5 of 10</span>
+                <span class="text-gray">Showing {{`${meta.from}-${meta.to}`}} of {{ pageSize }}</span>
             </div>
         </div>
     </div>
+
+    <el-dialog v-model="formVisible" :title="`${formTitle} Categories`" width="40%">
+        <div class="row">
+            <div class="col-6 form-group">
+                <label for="name">Name</label>
+                <input type="text" class="form-control" id="name" placeholder="Input category name" v-model="form.name">
+            </div>
+            <div class="col-6 form-group">
+                <label for="slug">Slug</label>
+                <input type="text" class="form-control" id="slug" placeholder="Input Slug" v-model="form.slug">
+            </div>
+        </div>
+        <template #footer>
+            <div class="dialog-footer d-flex justify-content-between">
+                <button class="btn btn-secondary text-white px-5" @click="formVisible = false">
+                    Cancel
+                </button>
+                <button v-if="formTitle === 'Add'" class="btn btn-blue-gradient text-white px-5" @click="handlePost()">
+                    {{ loading ? 'Saving...' : 'Save' }}
+                </button>
+                <button v-else class="btn btn-blue-gradient text-white px-5" @click="handlePut()">
+                    {{ loading ? 'Updating...' : 'Update' }}
+                </button>
+            </div>
+        </template>
+    </el-dialog>
+
+    <el-dialog v-model="detailVisible" title="Detail Categories" width="40%">
+        <table class="table" v-if="detailData">
+            <tbody>
+                <tr>
+                    <th>ID</th>
+                    <td>{{ detailData.id }}</td>
+                </tr>
+                <tr>
+                    <th>Name</th>
+                    <td>{{ detailData.name }}</td>
+                </tr>
+                <tr>
+                    <th>Slug</th>
+                    <td>{{ detailData.slug }}</td>
+                </tr>
+                <tr>
+                    <th>Log Info</th>
+                    <td>{{ formatDate(detailData.created_at) }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </el-dialog>
 </template>
 
 <script>
-import { ref } from 'vue'
-import { Search, Refresh, EditPen } from '@element-plus/icons-vue'
+import { ref, reactive, onMounted, watch } from 'vue'
+import { ElMessage } from 'element-plus'
+import { Search, Refresh, EditPen, View, Delete } from '@element-plus/icons-vue'
+import axios from 'axios'
+import { debounce, formatDate } from '@/utils/index.js'
 
 export default {
     name: 'categories',
     setup() {
+        const loading = ref(false)
+
+        const tableData = ref([])
+
+        const detailData = ref(null)
+
+        const totalData = ref(0)
+
+        const meta = ref({
+            from: 0,
+            to: 0
+        })
+
+        const formTitle = ref('Add')
+
         const multipleTableRef = ref(null)
 
         const multipleSelection = ref([])
@@ -108,65 +191,220 @@ export default {
 
         const currentPage = ref(1)
 
-        const pageSize = ref(10)
+        const pageSize = ref(5)
 
-        const pageOptions = [10, 25, 50]
+        const pageOptions = [5, 10, 25, 50]
 
         const handleSizeChange = (val) => {
-            console.log(`${val} items per page`)
+            pageSize.value = val
+            handleGetCategories()
         }
 
         const handleCurrentChange = (val) => {
-            console.log(`current page: ${val}`)
+            currentPage.value = val
+            handleGetCategories()
         }
 
-        const detailRow = (row) => {
-            console.log(row.id)
+        const formVisible = ref(false)
+
+        const detailVisible = ref(false)
+
+        const form = reactive({
+            name: '',
+            slug: '',
+        })
+
+        const handleGetCategories = async () => {
+            loading.value = true
+            await axios.get('/sanctum/csrf-cookie').then(async () => {
+                await axios.get('/api/categories', {
+                    params: {
+                        search: searchQuery.value,
+                        limit: pageSize.value,
+                        page: currentPage.value
+                    }
+                })
+                    .then((response) => {
+                        tableData.value = response.data.data.data
+                        totalData.value = response.data.data.total
+                        meta.value.from = response.data.data.from
+                        meta.value.to = response.data.data.to
+                    })
+                    .catch((error) => {
+                        console.log(error)
+                    })
+                    .finally(() => {
+                        loading.value = false
+                    })
+            })
         }
 
-        const deleteRow = (row) => {
-            console.log(row)
+        const handlePost = async () => {
+            loading.value = true
+            await axios.get('/sanctum/csrf-cookie').then(async () => {
+                await axios.post('/api/categories', form)
+                    .then((response) => {
+                        handleGetCategories()
+                        ElMessage({
+                            showClose: true,
+                            message: response.data.message,
+                            type: 'success',
+                        })
+                    })
+                    .catch((error) => {
+                        ElMessage({
+                            showClose: true,
+                            message: 'error',
+                            type: 'success',
+                        })
+                    })
+                    .finally(() => {
+                        loading.value = false
+                        formVisible.value = false
+                        form.name = ''
+                        form.slug = ''
+                    })
+            })
         }
 
-        const tableData = [
-            {
-                id: 1,
-                date: '2016-05-03',
-                name: 'Tom',
-            },
-            {
-                id: 2,
-                date: '2016-05-02',
-                name: 'Tom',
-            },
-            {
-                id: 3,
-                date: '2016-05-04',
-                name: 'Tom',
-            },
-            {
-                id: 4,
-                date: '2016-05-01',
-                name: 'Tom',
-            },
-        ]
+        const handlePut = async () => {
+            loading.value = true
+            await axios.get('/sanctum/csrf-cookie').then(async () => {
+                await axios.put(`/api/categories/${detailData.value.id}`, form)
+                    .then((response) => {
+                        handleGetCategories()
+                        ElMessage({
+                            showClose: true,
+                            message: response.data.message,
+                            type: 'success',
+                        })
+                    })
+                    .catch((error) => {
+                        ElMessage({
+                            showClose: true,
+                            message: 'error',
+                            type: 'success',
+                        })
+                    })
+                    .finally(() => {
+                        loading.value = false
+                        formVisible.value = false
+                        form.name = ''
+                        form.slug = ''
+                        detailData.value = null
+                    })
+            })
+        }
+
+        const handleDelete = async () => {
+            loading.value = true
+            const ids = multipleSelection.value.map((value) => value.id)
+            await axios.get('/sanctum/csrf-cookie').then(async () => {
+                await axios.patch(`/api/categories`, {
+                    ids: [ids]
+                })
+                    .then((response) => {
+                        handleGetCategories()
+                        ElMessage({
+                            showClose: true,
+                            message: response.data.message,
+                            type: 'success',
+                        })
+                    })
+                    .catch((error) => {
+                        ElMessage({
+                            showClose: true,
+                            message: 'error',
+                            type: 'success',
+                        })
+                    })
+                    .finally(() => {
+                        loading.value = false
+                    })
+            })
+        }
+
+        const detailRow = async (row) => {
+            loading.value = true
+            await axios.get('/sanctum/csrf-cookie').then(async () => {
+                await axios.get(`/api/categories/${row.id}`)
+                    .then((response) => {
+                        detailData.value = response.data.data
+                    })
+                    .catch((error) => {
+                        console.log(error)
+                    })
+                    .finally(() => {
+                        loading.value = false
+                        detailVisible.value = true
+                    })
+            })
+        }
+
+        const editRow = async (row) => {
+            loading.value = true
+            formTitle.value = "Edit"
+            await axios.get('/sanctum/csrf-cookie').then(async () => {
+                await axios.get(`/api/categories/${row.id}`)
+                    .then((response) => {
+                        detailData.value = response.data.data
+                        form.name = response.data.data.name
+                        form.slug = response.data.data.slug
+                    })
+                    .catch((error) => {
+                        console.log(error)
+                    })
+                    .finally(() => {
+                        loading.value = false
+                        formVisible.value = true
+                    })
+            })
+        }
+
+        onMounted(() => {
+            handleGetCategories()
+        })
+
+        watch(searchQuery, (value) => {
+            searchQuery.value = value
+            let returnedFunction = debounce(function() {
+                handleGetCategories()
+            }, 250);
+
+            returnedFunction()
+        })
 
         return {
+            formTitle,
             multipleTableRef,
+            multipleSelection,
             tableData,
+            totalData,
+            detailData,
             searchQuery,
             currentPage,
             pageOptions,
             pageSize,
+            formVisible,
+            detailVisible,
+            form,
+            loading,
+            meta,
+            handlePost,
+            handlePut,
+            handleDelete,
             handleSelectionChange,
             toggleSelection,
             handleCurrentChange,
             handleSizeChange,
             detailRow,
-            deleteRow,
+            editRow,
             Search,
             Refresh,
-            EditPen
+            EditPen,
+            View,
+            Delete,
+            formatDate
         }
     }
 }
